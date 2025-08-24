263547594e41540910125728804435f1a4b64d01
itshirdeshkkk <hirdesh.khandelwal@tedekstra.com>
2025-08-24 13:32:27 +0530

    fix: update the base URL

--- src/components/BaseURl/BaseUrl.js
+++ src/components/BaseURl/BaseUrl.js
@@ -1,5 +1,5 @@
-// export const BaseUrl = "https://testify-server-production.up.railway.app/api/";
-export const BaseUrl = "https://testify-server-tau.vercel.app/api/";
+export const BaseUrl = "https://testify-server-production.up.railway.app/api/";
+// export const BaseUrl = "https://testify-server-tau.vercel.app/api/";
 
 export const getAuthHeaders = () => {
     const token = sessionStorage.getItem("token");
