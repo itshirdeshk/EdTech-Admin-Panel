245780c493286f80c9a63beb1366cbf5a2d66e01
itshirdeshkkk <hirdesh.khandelwal@tedekstra.com>
2025-12-23 22:46:17 +0530

    fix: update the test page

--- src/components/BaseURl/BaseUrl.js
+++ src/components/BaseURl/BaseUrl.js
@@ -1,4 +1,4 @@
-export const BaseUrl = "https://testify-server-production-5d4a.up.railway.app/api/";
+export const BaseUrl = "https://testify-server-production-d2e4.up.railway.app/api/";
 // export const BaseUrl = "https://testify-server-tau.vercel.app/api/";
 
 export const getAuthHeaders = () => {
