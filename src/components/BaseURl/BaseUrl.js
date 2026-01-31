96d7e8f684e2a4648e650e19b0cddde994206147
itshirdeshkkk <hirdesh.khandelwal@tedekstra.com>
2026-01-31 11:52:33 +0530

    fix: update base URI

--- src/components/BaseURl/BaseUrl.js
+++ src/components/BaseURl/BaseUrl.js
@@ -1,4 +1,4 @@
-export const BaseUrl = "https://testify-server-production-d2e4.up.railway.app/api/";
+export const BaseUrl = "https://testify-server-production-98a5.up.railway.app/api/";
 // export const BaseUrl = "https://testify-server-tau.vercel.app/api/";
 // export const BaseUrl = "http://localhost:8080/api/";
 
